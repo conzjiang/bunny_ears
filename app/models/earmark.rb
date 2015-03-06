d11a81b90813c0da25cb10e75d7d367861ca19de
Constance Jiang <conzjiang@gmail.com>
2015-03-05 20:31:06 -0500

    Scrap earmarks. Add bunny icon.

--- app/models/earmark.rb
+++
@@ -1,5 +0,0 @@
-class Earmark < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true
-
-  has_many :earmarkings, inverse_of: :earmark
-end
--- app/models/earmarking.rb
+++
@@ -1,7 +0,0 @@
-class Earmarking < ActiveRecord::Base
-  validates :earmark, :tv_show_id, presence: true
-  validates :earmark, uniqueness: { scope: :tv_show_id }
-
-  belongs_to :earmark, inverse_of: :earmarkings
-  belongs_to :tv_show
-end
